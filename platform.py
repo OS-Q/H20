5ef517287eefcc2b9b369e0d42c537c145fde693
qitas <qitas@qitas.cn>
2021-05-18 10:50:25 +0800

    del

--- platform.py
+++
@@ -1,7 +0,0 @@
-
-from platform import system
-from platformio.managers.platform import PlatformBase
-
-class P117Platform(PlatformBase):
-    def configure_default_packages(self, variables, target):
-        return PlatformBase.configure_default_packages(self, variables,target)
